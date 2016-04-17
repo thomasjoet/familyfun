78b6073721cfb2825ca3edf2e4d6c012ca12ea55
Thomas Joët <thomas.joet.fr@gmail.com>
2016-04-17 14:29:05 +0200

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -19,4 +19,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "About | Familyfun"
   end
 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Familyfun"
+  end
+
 end
