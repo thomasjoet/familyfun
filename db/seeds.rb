0e5ef391c5786e4285b1477977e198d4e69b44a3
Thomas Joët <thomas.joet.fr@gmail.com>
2016-05-01 21:19:42 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(name:  "Tom",
+User.create!(name:  "Tom",
              email: "thomas.joet.fr@gmail.com",
              password:              "foobar",
              password_confirmation: "foobar",
@@ -14,6 +14,24 @@ User.create(name:  "Tom",
              activated: true,
              activated_at: Time.zone.now)
 
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+              email: email,
+              password:              password,
+              password_confirmation: password,
+              activated: true,
+              activated_at: Time.zone.now)
+end
+
 Book.create(title: "test", author: "test", description: "test", category: "test")
+
+users = User.order(:created_at).take(6)
+50.times do
+  comment = Faker::Lorem.sentence(5)
+  users.each { |user| user.readings.create!(comment: content, book_id: "1", grade: "4") }
+end
 
 end
